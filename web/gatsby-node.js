73446153e5429276f0adda7400e0ce2641087560
StudioCC17 <72558882+StudioCC17@users.noreply.github.com>
2020-12-17 14:26:53 +0000

    Add files via upload

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -6,43 +6,6 @@ const { format } = require('date-fns')
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-async function createBlogPostPages(graphql, actions, reporter) {
-  const { createPage } = actions
-  const result = await graphql(`
-    {
-      allSanityPost(filter: { slug: { current: { ne: null } } }) {
-        edges {
-          node {
-            id
-            publishedAt
-            slug {
-              current
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  if (result.errors) throw result.errors
-
-  const postEdges = (result.data.allSanityPost || {}).edges || []
-
-  postEdges.forEach((edge, index) => {
-    const { id, slug = {}, publishedAt } = edge.node
-    const dateSegment = format(publishedAt, 'YYYY/MM')
-    const path = `/blog/${dateSegment}/${slug.current}/`
-
-    reporter.info(`Creating blog post page: ${path}`)
-
-    createPage({
-      path,
-      component: require.resolve('./src/templates/blog-post.js'),
-      context: { id }
-    })
-  })
-}
-
 async function createProjectPages(graphql, actions, reporter) {
   const { createPage } = actions
   const result = await graphql(`
